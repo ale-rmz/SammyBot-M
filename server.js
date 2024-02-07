2b9f1aae60a4bd55d12adaa6f7841f0bd172a332
JOSTINofc <159198564+JOSTINofc@users.noreply.github.com>
2024-02-07 08:39:26 -0500

    𝑮𝒐𝒌𝒖𝑩𝒐𝒕𝑳𝒊𝒕𝒆-𝑴𝑫
    Signed-off-by: JOSTINofc <159198564+JOSTINofc@users.noreply.github.com>

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function connect(conn, PORT) {
   const app = global.app = express();
   console.log(app);
   const server = global.server = createServer(app);
-  let _qr = 'QR invalido, probablemente ya hayas escaneado el QR.';
+  let _qr = 'QR invalido, probablemente ya hayas escaneado el QR .';
 
   conn.ev.on('connection.update', function appQR({qr}) {
     if (qr) _qr = qr;
