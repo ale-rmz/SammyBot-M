c082def8e0e9c9628a031e95175cb03dd0d74737
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-25 21:07:14 -0400

    Actualizar plugins

---
+++ plugins/Apk-edits.js
@@ -0,0 +1,22 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.ApkEdits);
+};
+handler.command = /^(ApkEdits|Apkdzn)$/i;
+export default handler;
+global.ApkEdits = `
+𝘼𝙋𝙆 𝘿𝙕𝙉 𝘿𝙔𝙇𝘼𝙉
+
+
+PsTouch :
+ https://drive.google.com/file/d/1wXdGQ6bk_A6qlW582BIxu7MtQ0WYO60D/view
+
+
+PixelLab :
+https://drive.google.com/file/d/1wPwgnjT8NYfjpZjoaIoxls4dhdqh56qr/view?usp=drive_link
+
+
+IbisPaint :
+https://play.google.com/store/apps/details?id=jp.ne.ibis.ibispaintx.app
+
+𝙱𝚈: 𝙳𝚈𝙻𝙰𝙽-𝙱𝙾𝚃
+`;
