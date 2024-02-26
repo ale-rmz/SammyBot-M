29b813924a6615bdd9ccc818d9a039955fb3af4a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-25 21:04:51 -0400

    Actualizar plugins

---
+++ plugins/Drive-edits.js
@@ -0,0 +1,46 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.DriveEdits);
+};
+handler.command = /^(DriveEdits|drivesdzn|drivesdzndylan|dzndylan)$/i;
+export default handler;
+global.DriveEdits = `
+𝘿𝙍𝙄𝙑𝙀𝙎 𝘿𝙕𝙉
+
+DRIVES EDITS :
+https://drive.google.com/drive/folders/148_l0Rxkfmm9AWMxwN-oMG4x2706whAv
+
+https://drive.google.com/drive/folders/1RMZdVco83CQ9X81gDDovVO5SZNU2BZXb
+
+https://drive.google.com/drive/mobile/folders/15hjIAocSCZdK4v-nSgRb2Pp78rCMBMXY/1-PPXNyh0M51SusMSVPNF1d-Q7EkZUQDW/11ri1_tdrZP86PH8dWZZI2yFVPpH9PSgV?sort=13&direction=a
+
+https://drive.google.com/drive/u/0/mobile/folders/1kWvht4sSR0ybvlZiI0SAIitCIrNgK_wA
+
+
+DEGRADADOS :
+https://mega.nz/folder/LaI0HBJa#7_EDAma5WSTqMjEFDPvWyw
+
+
+DESTACADAS :
+https://drive.google.com/drive/folders/1SBkTyNjr2ZvJoSc7oCZfe6AzwSSdFgUG
+
+
+EMPRESAS :
+https://mega.nz/folder/OeR3iKjZ#pgi4Ka5I5b6bMrgAGwVsNw
+
+
+FONDOS :
+https://mega.nz/folder/DOgQFbhC#zQPYN3J9776vxuclCkNAwg
+
+
+DRIVE FEM :
+https://drive.google.com/drive/mobile/folders/17qXegvUYmXU0nYnMUJKJTSzGcChjQXyH
+
+
+LAMPARAS :
+https://mega.nz/folder/OPB1ESxS#1HxyBc_6dbNvYZxxIkwxbQ
+
+
+LINEAS :
+https://mega.nz/folder/7epCiSbb#Vpx3e3Q5Aws-F-eOuE2O6Q
+
+`;
