a15dc79667bdf9e27f6cd8b8a874703c32f5eb4c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-26 19:55:44 -0400

    Actualizar Spam-Apk.js

--- plugins/Spam-Apk.js
+++ plugins/Spam-Apk.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   m.reply(global.SpamApk);
     const pp = imagen5;
 };
-handler.command = /^(SpamApk)$/i;
+handler.command = /^(SpamApk|Apkspam)$/i;
 export default handler;
 global.SpamApk = `
 𝐋𝐈𝐍𝐊 𝐒𝐏𝐀𝐌 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 : https://www.mediafire.com/file/elvm4t2m0tf25ux/SPAM_WHATSAPP.apk/file
