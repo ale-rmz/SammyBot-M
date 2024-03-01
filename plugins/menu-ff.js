da32c65ca8ae1d276305778e0520c65264665532
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-29 21:38:08 -0400

    Actualizar menu-ff.js

--- plugins/menu-ff.js
+++ plugins/menu-ff.js
@@ -89,66 +89,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 │➼💻 𝐌𝐚𝐬𝐜𝟖𝐯𝐬𝟖
 │➼💻 𝐌𝐚𝐬𝐜𝟏𝟐𝐯𝐬𝟏𝟐
 │➼💻 𝐌𝐚𝐬𝐜𝟏𝟔𝐯𝐬𝟏𝟔
-╰──────────────────╯
-
-
-   *╔══ ≪ ⚠️ *FREE FIRE* ⚠️ ≫ ══╗*
-    *║*
-    *║* ≪ PUEDEN USAR LOS ADMINS ≫
-    *║*
-    *║* ≪ REGLAS , LISTA , FICHA ≫
-    *║*
-    *║* 💻➺ _${usedPrefix}Fem4_
-    *║* 💻➺ _${usedPrefix}Fem6_
-    *║* 💻➺ _${usedPrefix}FemInterna4_
-    *║* 💻➺ _${usedPrefix}FemInterna6_
-    *║* 💻➺ _${usedPrefix}Masc4_
-    *║* 💻➺ _${usedPrefix}Masc6_
-    *║* 💻➺ _${usedPrefix}MascInterna4_
-    *║* 💻➺ _${usedPrefix}MascInterna6_
-    *║* 💻➺ _${usedPrefix}Mixto4_
-    *║* 💻➺ _${usedPrefix}Mixto6_
-    *║* 💻➺ _${usedPrefix}MixtoInterna4_
-    *║* 💻➺ _${usedPrefix}MixtoInterna6_
-    *║* 💻➺ _${usedPrefix}lista4_
-    *║* 💻➺ _${usedPrefix}apos_
-    *║* 💻➺ _${usedPrefix}mapa_
-    *║* 💻➺ _${usedPrefix}Ficha / Formulario Reclutamiento_
-    *║* 💻➺ _${usedPrefix}Nexterra_
-    *║* 💻➺ _${usedPrefix}Bermuda_
-    *║* 💻➺ _${usedPrefix}Kalahary_
-    *║* 💻➺ _${usedPrefix}Alpes_
-    *║* 💻➺ _${usedPrefix}AgendaSemanal_
-    *╚══ ≪ •❈• ≫ ══╝*
-
-    ╭━〔 ⚔️ *BUSCAR VS PLANTILLAS* 🔱 〕
-    *┃➤ 🌀 ${usedPrefix}buscarvsmasc*
-    *┃➤ ⚔️ ${usedPrefix}bsmsac*
-    *┃➤ ⚓ ${usedPrefix}buscarvsfem*
-    *┃➤ ✨ ${usedPrefix}bsfem*
-    *┃➤ 📍 ${usedPrefix}buscarvsmixto*
-    *┃➤ 👁️‍🗨️ ${usedPrefix}bsmixto*
-     ╰━━━━━━━━━━━━
-
-     *╔══ ≪ LINKS DE COMUS/APK Y DRIVES DZN ≫ ══╗*
-    *║* 🎐_${usedPrefix}SpamApk / WhatsApp_
-    *║* 🎐_${usedPrefix}SpamComu / Comunidades_
-    *║* 🎐 _${usedPrefix}Spam2_
-    *║* 🎐 _${usedPrefix}rcursosdzn_
-    *║* 🎐 _${usedPrefix}drivesdzn_
-    *║* 🎐 _${usedPrefix}dzn_
-    *║* 🎐 _${usedPrefix}ApkEdits_
-    *║* 🎐 _${usedPrefix}DriveEdits_
-    *╚══ ≪ •❈• ≫ ══╝*
-    
-    ╭━〔 ⚠️ *MAPAS* ⚠️ 〕
-    *┃➤ 🗺️ ${usedPrefix}bermuda*
-    *┃➤ 🗺️ ${usedPrefix}kalahari*
-    *┃➤ 🗺️ ${usedPrefix}alpes*
-    *┃➤ 🗺️ ${usedPrefix}purgatorio*
-    *┃➤ 🗺️ ${usedPrefix}nexterra*
-    *┃➤ 📋 ${usedPrefix}agendasemanal*
-     ╰━━━━━━━━━━━━`.trim();
+╰──────────────────╯`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
