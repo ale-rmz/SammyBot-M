f09da1fc221a73b80156daad0a3f68704c1e043d
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-04-11 18:20:51 -0300

    owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, isROwner, text}) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
   if (conn.user.jid == conn.user.jid) {
     // conn.readMessages([m.key])
-    await m.reply('[🐺] 𝗠𝗲 𝗲𝘀𝘁𝗼𝘆 𝗿𝗲𝗶𝗻𝗶𝗰𝗶𝗮𝗻𝗱𝗼,𝗲𝘀𝗽𝗲𝗿𝗲 𝘂𝗻𝗼𝘀 𝘀𝗲𝗴𝘂𝗻𝗱𝗼𝘀 𝗽𝗼𝗿 𝗳𝗮𝘃𝗼𝗿 ... ');
+    await m.reply('[🧸] 𝗠𝗲 𝗲𝘀𝘁𝗼𝘆 𝗿𝗲𝗶𝗻𝗶𝗰𝗶𝗮𝗻𝗱𝗼,𝗲𝘀𝗽𝗲𝗿𝗲 𝘂𝗻𝗼𝘀 𝘀𝗲𝗴𝘂𝗻𝗱𝗼𝘀 𝗽𝗼𝗿 𝗳𝗮𝘃𝗼𝗿 ... ');
     process.send('reset');
   } else throw 'eh';
 };
