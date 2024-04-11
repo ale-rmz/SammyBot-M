105a88451cdcfef634142dfecab65c7f3a4b5b6f
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-04-11 18:39:52 -0300

    owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,12 +1,12 @@
 const handler = async (m, {conn, participants, usedPrefix, command}) => {
-  const BANtext = `[⛔] 𝐎𝐰𝐧𝐞𝐫, 𝐫𝐞𝐬𝐩𝐨𝐧𝐝𝐚 𝐨 𝐞𝐭𝐢𝐪𝐮𝐞𝐭𝐞 𝐚𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐚 𝐛𝐚𝐧𝐞𝐚𝐫\n\n—◉ 𝐄𝐣𝐞𝐦𝐩𝐥𝐨:\n*${usedPrefix + command} @${global.suittag}*`;
+  const BANtext = `[⛔] 𝙊𝙬𝙣𝙚𝙧,𝙧𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙤 𝙚𝙩𝙞𝙦𝙪𝙚𝙩𝙚 𝙚𝙡 𝙪𝙨𝙖𝙧𝙞𝙤 𝙖 𝙗𝙖𝙣𝙚𝙖𝙧 \n\n—◉ 𝙀𝙟𝙚𝙢𝙥𝙡𝙤:\n*${usedPrefix + command} @${global.suittag}*`;
   if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, {mentions: conn.parseMention(BANtext)});
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
   else who = m.chat;
   const users = global.db.data.users;
   users[who].banned = true;
-  m.reply('[🌼] 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨, 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐛𝐚𝐧𝐞𝐚𝐝𝐨 𝐜𝐨𝐧 𝐞𝐱𝐢𝐭𝐨, 𝐞𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐧𝐨 𝐩𝐨𝐝𝐫𝐚 𝐮𝐬𝐚𝐫 𝑮𝒐𝒌𝒖𝑩𝒐𝒕𝑳𝒊𝒕𝒆-𝑴𝑫🍀');
+  m.reply('[🫀]𝙋𝙚𝙧𝙛𝙚𝙘𝙩𝙤,𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙗𝙖𝙣𝙚𝙖𝙙𝙤 𝙘𝙤𝙣 𝙚𝙭𝙞𝙩𝙤,𝙚𝙡 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙣𝙤 𝙥𝙤𝙙𝙧𝙖 𝙪𝙨𝙖𝙧 𝙎𝙖𝙢𝙢𝙮𝘽𝙤𝙩-𝙈𝘿🧸');
 };
 handler.command = /^prohibiruser$/i;
 handler.rowner = true;
