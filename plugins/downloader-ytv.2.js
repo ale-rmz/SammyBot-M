99191066b3a35f530e9bae5ed04dfdafd1934082
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-04-12 11:24:31 -0300

    downloader-ytv.2.js

--- plugins/downloader-ytv.2.js
+++ plugins/downloader-ytv.2.js
@@ -2,7 +2,7 @@ import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, args}) => {
   if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*';
-  await m.reply(`*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*\n\n*◉ Sɪ Sᴜ ᴠɪᴅᴇᴏ ɴᴏ ᴇs ᴇɴᴠɪᴀᴅᴏ, ᴘʀᴜᴇʙᴇ ᴄᴏɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`);
+  await m.reply(`*_⏳*𝖲𝖾 𝖾𝗌𝗍𝖺 𝗉𝗋𝗈𝖼𝖾𝗌𝖺𝗇𝖽𝗈 𝗌𝗎 𝗏𝗂𝖽𝖾𝗈...⏳_*\𝗇\𝗇 𝖲𝗂 𝗌𝗎 𝗏𝗂𝖽𝖾𝗈 𝗇𝗈 𝖾𝗌 𝖾𝗇𝗏𝗂𝖺𝖽𝗈, 𝗉𝗋𝗎𝖾𝖻𝖾 𝖼𝗈𝗇 𝗅𝗈𝗌 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌 #playdoc , #play.2 , #ytmp4doc.`);
   try {
     const qu = args[1] || '360';
     const q = qu + 'p';
@@ -11,7 +11,7 @@ const handler = async (m, {conn, args}) => {
     const dl_url = await yt.video[q].download();
     const ttl = await yt.title;
     const size = await yt.video[q].fileSizeH;
-    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}`.trim();
+    const cap = `*◉—⌈📥 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗗𝗟 📥⌋—◉*\n❏ *𝖳𝖨𝖳𝖴𝖫𝖮:* ${ttl}\n❏ *𝖯𝖤𝖲𝖮:* ${size}`.trim();
     await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
   } catch {
     try {
@@ -20,7 +20,7 @@ const handler = async (m, {conn, args}) => {
       const n = lolh.result.title || 'error';
       const n2 = lolh.result.link;
       const n3 = lolh.result.size;
-      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}`.trim();
+      const cap2 = `*◉—⌈📥 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗗𝗟 📥⌋—◉*\n❏ *𝖳𝖨𝖳𝖴𝖫𝖮:* ${n}\n❏ *𝖯𝖤𝖲𝖮:* ${n3}`.trim();
       await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m});
     } catch {
       await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*', m);
